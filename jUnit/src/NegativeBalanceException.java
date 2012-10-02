2a72e6e34946afb1ac5fe7e81b2c8fcf35b9005f
Ryan McManus <rmcmanus@mines.edu>
2012-10-02 08:22:52 -0600

    Adding NegativeBalanceException.java

---
+++ jUnit/src/NegativeBalanceException.java
@@ -0,0 +1,13 @@
+
+public class NegativeBalanceException extends Exception {
+
+	public NegativeBalanceException(String message) {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "NegativeBalanceException []";
+	}
+	
+}
